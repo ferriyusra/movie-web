36f3000ce20f4c66a7dbd4f63bd28c917e115bf6
ferriyusra <ferisakuragi@gmail.com>
2025-02-06 15:59:11 +0700

    feat: landing page ui

--- src/components/views/Home/Home.tsx
+++ src/components/views/Home/Home.tsx
@@ -1,9 +1,38 @@
+import { Skeleton } from "@nextui-org/react";
+import HomeEventList from "./HomeEventList";
+import HomeSlider from "./HomeSlider";
+import useHome from "./useHome";
+import Image from "next/image";
+import HomeCategoryList from "./HomeCategoryList";
+
 const Home = () => {
+  const {
+    dataBanners,
+    isLoadingBanners,
+    dataFeaturedEvents,
+    isLoadingFeaturedEvents,
+    dataLatestEvents,
+    isLoadingLatestEvents,
+    dataCategories,
+    isLoadingCategories,
+  } = useHome();
   return (
     <div>
-      <div>home</div>
+      <HomeSlider
+        banners={dataBanners?.data}
+        isLoadingBanners={isLoadingBanners}
+      />
+      <HomeEventList
+        title="Acara Unggulan"
+        events={dataFeaturedEvents?.data}
+        isLoading={isLoadingFeaturedEvents}
+      />
+      <HomeCategoryList
+        categories={dataCategories?.data}
+        isLoading={isLoadingCategories}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
