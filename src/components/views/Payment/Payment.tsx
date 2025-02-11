cc0d72b2f4209ff3054261cbc61c61acc3666347
ferriyusra <ferisakuragi@gmail.com>
2025-02-11 17:22:53 +0700

    refactor

--- src/components/views/Payment/Payment.tsx
+++ src/components/views/Payment/Payment.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@nextui-org/react";
-import router, { useRouter } from "next/router";
+import { Button } from "@heroui/react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import usePayment from "./usePayment";
 import { useEffect } from "react";
 
 const Payment = () => {
   const router = useRouter();
-  const { order_id, status } = router.query;
   const { mutateUpdateOrderStatus } = usePayment();
+  const { order_id, status } = router.query;
 
   useEffect(() => {
     if (router.isReady) {
       mutateUpdateOrderStatus();
     }
-  }, [router.isReady])
-
+  }, [router.isReady]);
   return (
     <div className="flex w-screen flex-col items-center justify-center gap-10 p-4">
       <div className="flex flex-col items-center justify-center gap-10">
@@ -26,7 +25,7 @@ const Payment = () => {
         />
         <Image
           src={
-            status === "success"
+            router.query.status === "success"
               ? "/images/illustrations/success.svg"
               : "/images/illustrations/pending.svg"
           }
@@ -36,21 +35,16 @@ const Payment = () => {
         />
       </div>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-3xl font-bold text-danger-500 capitalize">
+        <h1 className="text-3xl font-bold capitalize text-danger-500">
           Transaction {status}
         </h1>
-        <p className="text-xl font-bold text-default-500 capitalize">
-          {status === "success"
-            ? "Yeay, kamu telah berhasil melakukan pembayaran di Acara"
-            : "Ups, pembayaran kamu gagal"}
-        </p>
         <Button
           className="mt-4 w-fit"
           variant="bordered"
           color="danger"
           onPress={() => router.push(`/member/transaction/${order_id}`)}
         >
-          Cek transaksi kamu yuk !
+          Check your transaction here
         </Button>
       </div>
     </div>
